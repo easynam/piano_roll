886f9356c42eace5f95e925e6605a5e2e5819177
John Chadwick <john@jchw.io>
2020-12-30 06:43:29 -0800

    Implement preliminary demo with CPAL + redoxsynth

--- src/audio/mod.rs
+++ src/audio/mod.rs
@@ -1,6 +1,8 @@
-use std::sync::mpsc::{Receiver, SyncSender, channel, sync_channel};
+use std::{sync::mpsc::{Receiver, SyncSender, sync_channel}};
 use std::thread::sleep;
 use std::time::Duration;
+
+use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
 
 pub enum Command {
     Play,
@@ -23,12 +25,46 @@ impl Synth {
         )
     }
 
+    pub fn test_run<T>(device: cpal::Device, config: &cpal::StreamConfig)
+    where
+        T: cpal::Sample,
+    {
+        let sample_rate = config.sample_rate.0 as f32;
+        let settings = redoxsynth::Settings::new().unwrap();
+        let mut synth = redoxsynth::Synth::new(settings).unwrap();
+        synth.set_sample_rate(sample_rate);
+        synth.sfload("gm.sf2", true).unwrap();
+        synth.note_on(0, 60, 127).unwrap();
+        synth.note_on(0, 64, 127).unwrap();
+
+        let err_fn = |err| eprintln!("an error occurred on stream: {}", err);
+
+        let stream = device.build_output_stream(
+            config,
+            move |data: &mut [f32], _: &cpal::OutputCallbackInfo| {
+                synth.write(data).unwrap();
+            },
+            err_fn,
+        ).unwrap();
+        stream.play().unwrap();
+
+        std::thread::sleep(std::time::Duration::from_millis(1000));
+    }
+
     pub fn run(self) {
         loop {
             sleep(Duration::from_millis(100));
             while let Ok(command) = self.recv.try_recv() {
                 match command {
-                    Command::Play => println!("play!"),
+                    Command::Play => {
+                        let host = cpal::default_host();
+                        let device = host.default_output_device().unwrap();
+                        Synth::test_run::<f32>(device, &cpal::StreamConfig{
+                            channels: 2,
+                            sample_rate: cpal::SampleRate(48000),
+                            buffer_size: cpal::BufferSize::Default,
+                        });
+                    }
                     Command::Stop => println!("stop!"),
                 }
             }
