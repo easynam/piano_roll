ef0d0885e4f9bdbec15319aa43682a5b62b29e63
John Chadwick <john@jchw.io>
2021-01-04 10:00:27 -0800

    Update mixer for Rust 1.5x
    (also remove outdated comment.)

--- src/audio/mixer.rs
+++ src/audio/mixer.rs
@@ -20,18 +20,12 @@ impl Mixer {
 impl Source for Mixer {
     fn output_audio(&mut self, mut sample: usize, output: &mut [f32]) {
         for output_chunk in output.chunks_mut(self.buffer.len()) {
-            // Replace with output_chunk.fill(Default::default()) in Rust 1.50.
-            output_chunk
-                .iter_mut()
-                .for_each(|f| *f = Default::default());
+            output_chunk.fill(Default::default());
 
             for source in self.sources.iter_mut() {
                 let buffer_chunk = &mut self.buffer[0..output_chunk.len()];
                 source.output_audio(sample, buffer_chunk);
 
-                // TODO: This is ugly. Should figure out how to best handle
-                // mixing when not using f32, or perhaps simply always use f32
-                // internally.
                 output_chunk
                     .iter_mut()
                     .zip(buffer_chunk)
